1e9b3aa2f4766537345e7cb9e2c0ffbfbc340748
bryan-br0n <126531322+bryan-br0n@users.noreply.github.com>
2023-11-20 18:12:03 -0600

    Update models.py
    modelo contacto finalizado

--- productos/models.py
+++ productos/models.py
@@ -13,4 +13,8 @@ class Producto(models.Model):
    
     
 class Contacto(models.Model):
- 
+    id = models.AutoField(primary_key=True, auto_created=True)
+    nombre = models.TextField(null=False, max_length=50,verbose_name="Nombre")
+    email = models.EmailField(null=False, max_length=100,verbose_name="Correo Electrónico")
+    Mensaje = models.TextField(null=False, max_length=200,verbose_name="Mensaje")
+
