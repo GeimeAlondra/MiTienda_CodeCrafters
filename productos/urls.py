1da3051a05ab2693fc420c09ee069ccb99be8732
bryan-br0n <126531322+bryan-br0n@users.noreply.github.com>
2023-11-20 18:13:01 -0600

    Update urls.py
    creacion de rutas para utilizarlas entre los metodos y la vista

--- productos/urls.py
+++ productos/urls.py
@@ -2,6 +2,24 @@ from django.urls import path
 from productos import views
 
 urlpatterns = [
+    path('', views.inicio, name="inicio"),
+    path('productos/', views.listado_productos,name="productos"),
+    path('productos/crear', views.crearProducto,name="crear_producto"),
+    path('productos/editar/<int:id>', views.editarProducto,name="editar_producto"),
+    path('productos/eliminar/<int:id>', views.eliminar_producto, name="eliminar_producto"),
+    
+    #Contacto
+    path('contactos/', views.listado_contactos,name="contactos"),
+    path('Contactanos', views.crearContacto,name="contacto"),
+    path('contactos/editar/<int:id>', views.editarContacto, name="editar_contacto"),
+    path('contactos/eliminar/<int:id>', views.eliminar_contacto, name="eliminar_contacto"),
+    
+    #Acerca de
+    path('acercaDe/', views.acerca_de, name="acercaDe"),
+    
+    #Carrito de compra
+    path('carrito/<int:id>', views.vistaCarrito, name="carrito"),
+    path('carrito/elimanar/<int:id>', views.vistaDeleteCarrito, name="carrito_delete"),
     
     
 ]
